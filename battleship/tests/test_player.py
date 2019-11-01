333eeacd6a1268da7642fcd6e3410b3d3d1db8bd
Jonathan Cobian <jcobian@civisanalytics.com>
2019-11-01 15:21:29 -0700

    remove tests for old fn

--- battleship/tests/test_player.py
+++ battleship/tests/test_player.py
@@ -23,29 +23,6 @@ def test_player_repr(player_obj):
     assert str(player_obj) == "Jeff Probst"
 
 
-@mock.patch.object(player.HumanPlayer, '_ask_user_for_input')
-def test_player_pick_move(mock_ask_user, player_obj):
-    mock_ask_user.return_value = (1, 2)
-    other_board = mock.MagicMock()
-    other_board.is_valid_move.return_value = (True, None)
-    assert player_obj.pick_move(other_board) == (1, 2)
-
-
-@mock.patch.object(player.HumanPlayer, '_ask_user_for_input')
-def test_player_pick_move_if_move_invalid(mock_ask_user, player_obj):
-    mock_ask_user.return_value = (1, 2)
-    other_board = mock.MagicMock()
-    other_board.is_valid_move.return_value = (False, 'some error')
-    with pytest.raises(errors.InvalidMoveError) as excinfo:
-        player_obj.pick_move(other_board)
-        assert 'some error' in str(excinfo.value)
-
-
-def test_player_pick_move_if_board_invalid(player_obj):
-    with pytest.raises(errors.InvalidBoardError):
-        player_obj.pick_move(player_obj.board)
-
-
 def test_cpu_player_repr():
     mock_board = mock.MagicMock()
     p = player.CPUPlayer(mock_board, "Jack Sparrow")
